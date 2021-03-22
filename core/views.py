fd9617d757782cbcdab9727304482e6185291d4d
Cezar Roberto Poeta <cezarrobertopoeta@gmail.com>
2021-03-21 21:16:03 -0300

    Forms
    Forms connected with database

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 
-from .forms import ContatoForm
+from .forms import ContatoForm, ProdutoModelForm
 
 def index(request):
     return render(request, 'index.html')
@@ -27,5 +27,26 @@ def contato(request):
 
 
 def produto(request):
-    return render(request, 'produto.html')
+    if str(request.method) == 'POST':
+        form = ProdutoModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            prod = form.save(commit=False)
+
+            print(f'Nome: {prod.nome}')
+            print(f'Preco: {prod.preco}')
+            print(f'Estoque: {prod.estoque}')
+            print(f'Imagem: {prod.imagem}') 
+
+            messages.success(request, 'Produto Salvo com Sucesso')
+            form = ProdutoModelForm()
+        else:
+            messages.error(request, 'Erro ao Salvar Produto')
+    else:
+        form = ProdutoModelForm()
+    
+    # deve estar fora do if
+    context = {
+        'form': form
+    }
+    return render(request, 'produto.html', context)
 
